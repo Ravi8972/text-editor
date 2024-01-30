cfaab3e3d40da1bd1d54fde45dea1a34d2c2e1d0
Raviranjan Mishra <ravimishra.rm9558@gmail.com>
2024-01-30 13:48:17 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const [text,settext] = useState("");
+  const [history,setHistory]= useState([]);
+
+  function Undo(){
+    if(history.length>0){
+    let x = history[history.length-1];
+    setHistory(history.slice(0,history.length-1));
+    settext(x);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="Text"  value={text} onChange={e=> settext(e.target.value)}/>
+     <button onClick={() =>{
+      setHistory([...history, text]);
+      settext("")
+     }}>Append</button>
+    <button onClick={Undo}> undo </button>
+
+    <div>
+      {history.map((item ,index) => <p key={index}>{item} {item}</p>) }
+    </div>
+
     </div>
   );
 }
